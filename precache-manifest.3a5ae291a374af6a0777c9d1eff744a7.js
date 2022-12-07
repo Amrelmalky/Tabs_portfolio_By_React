4ddaaccc29b4c4c25fe854ea1958372b0c6c33e4
Amrelmalky <69693889+Amrelmalky@users.noreply.github.com>
2022-12-07 23:28:05 +0200

    Updates

---
+++ precache-manifest.3a5ae291a374af6a0777c9d1eff744a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ba1cd411b719a39dbbe4873271bcf8",
+    "url": "/Tabs_portfolio_By_React/index.html"
+  },
+  {
+    "revision": "968fd7f9108f6e048c15",
+    "url": "/Tabs_portfolio_By_React/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "40136ac5e1bf6f81ac22",
+    "url": "/Tabs_portfolio_By_React/static/js/2.4e540277.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tabs_portfolio_By_React/static/js/2.4e540277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "968fd7f9108f6e048c15",
+    "url": "/Tabs_portfolio_By_React/static/js/main.c31fd1e1.chunk.js"
+  },
+  {
+    "revision": "dcdc21cc0997bf797ddd",
+    "url": "/Tabs_portfolio_By_React/static/js/runtime-main.f6b464e6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Tabs_portfolio_By_React/precache-manifest.3a5ae291a374af6a0777c9d1eff744a7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Tabs_portfolio_By_React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
